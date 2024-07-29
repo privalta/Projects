b8295143e091471eb3ad9119e951394a979938d6
Rafael Rubén Jorge González <rrjorgegz@gmail.com>
2024-07-29 17:42:06 -0400

    [IMP] add depends config account

--- rapidusa/__manifest__.py
+++ rapidusa/__manifest__.py
@@ -33,5 +33,5 @@
     "installable": True,
     "auto_install": False,
     "application": True,
-    "depends": ["base", "hr", "payroll_account"],
+    "depends": ["base", "hr", "payroll_account", "l10n_generic_coa"],
 }
